bc0e4e3400e4383bf0623bb3cd1ad35ed7f4d3d5
loremm34 <161493152+loremm34@users.noreply.github.com>
2024-03-12 21:03:24 +0000

    complete program

--- .ccls-cache/@home@runner@lab3task1gr13yevdokimov/src@main.c
+++ .ccls-cache/@home@runner@lab3task1gr13yevdokimov/src@main.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 
 // Function to check if a number is even
-int is_even(int num) {
-    return num % 2 == 0;
-}
+int is_even(int num) { return num % 2 == 0; }
 
 int count_even(int *sequence, int length) {
-    int count = 0;
-    for (int i = 0; i < length; i++) {
-        if (is_even(sequence[i])) {
-            count++;
-        }
+  int count = 0;
+  for (int i = 0; i < length; i++) {
+    if (is_even(sequence[i])) {
+      count++;
     }
-    return count;
+  }
+  return count;
+}
+
+int count_odd(int *sequence, int length) {
+  int count = 0;
+  for (int i = 0; i < length; i++) {
+    if (!is_even(sequence[i])) {
+      count++;
+    }
+  }
+  return count;
+}
+
+int main() {
+  int a[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+  int b[8] = {9, 8, 7, 6, 5, 4, 3, 2};
+
+  printf("Количество четных чисел в первой последовательности: %d\n",
+         count_even(a, 8));
+  printf("Количество нечетных чисел во второй последовательности: %d\n",
+         count_odd(b, 8));
+
+  return 0;
 }
